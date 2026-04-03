4e9e5f2a4dfafc4910f06d665d4246f2cf6a7e16
BEYALB <seu_beyalb_github@exemplo.com>
2026-04-03 19:06:50 -0300

    esta pronto até o botão voltar

--- config/process.php
+++ config/process.php
@@ -4,6 +4,25 @@ session_start();
 
 include_once 'config/url.php';
 include_once 'config/connection.php';
+$id;
+//esta invertido true se existirem parametros na url
+if(!empty($_GET)){
+    $id =$_GET['id'];
+ 
+}
+// Retorna dados de um contato
+if(!empty($id)){
+
+    $query = "SELECT * FROM contacts WHERE id = :id";
+    $pronto= $conn->prepare($query);
+    $pronto->bindParam(':id',$id);
+    $pronto->execute();
+
+    $contacts = $pronto->fetch();
+
+}else{
+
+//Retorna todos  os contatos 
 $contacts = [];
 
 $query = "SELECT * FROM contacts";
@@ -12,5 +31,6 @@ $stmt= $conn->prepare($query);
 $stmt->execute();
 
 $contacts = $stmt->fetchAll();
+}
 
 ?>
