df2d286c2382372675721834f828fe89559d78f3
BEYALB <seu_beyalb_github@exemplo.com>
2026-04-05 21:47:04 -0300

    continuando trabalhando

--- config/process.php
+++ config/process.php
@@ -2,8 +2,42 @@
 
 session_start();
 
-include_once 'config/url.php';
-include_once 'config/connection.php';
+include_once 'connection.php';
+include_once 'url.php';
+
+$data= $_POST;
+//MODIFICÇÕES NO BANCO
+//verifica se existe dados no post 
+if(!empty($data)){
+ // print_r($data); exit;
+  //criar contato
+   if($data['type']==='create'){
+    $name = $data['name'];
+    $phone = $data['phone'];
+    $observations = $data['observations'];
+
+    $query = "INSERT INTO contacts (name, phone, observations) VALUES (:name, :phone, :observations)";
+    $stmt = $conn->prepare($query);
+    $stmt->bindParam(':name', $name);
+    $stmt->bindParam(':phone', $phone);
+    $stmt->bindParam(':observations', $observations);
+    try
+    {
+        $stmt->execute();
+        $_SESSION['msg'] = "Contato criado com sucesso!";
+
+    
+   }catch(PDOException $e){
+        $_SESSION['msg'] = "Erro ao criar contato: " . $e->getMessage();
+   }
+    //header("Location:" . $BASE_URL . "../index.php");
+   }
+   // redirect
+   header("Location:" . $BASE_URL . "../index.php");
+
+   // SELECÇÃO DE DADOS
+}else{
+    
 $id;
 //esta invertido true se existirem parametros na url
 if(!empty($_GET)){
@@ -32,5 +66,10 @@ $stmt->execute();
 
 $contacts = $stmt->fetchAll();
 }
+}
+
+//fechar conexao
+
+$conn = null;
 
 ?>
